fa3964f3d1b0b501942ac371266d22d893d25d19
igor-minaev <igor.minayeu@gmail.com>
2022-06-07 21:01:46 +0300

    start task

---
+++ src/p2-homeworks/h1/Message.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+type MessageType = {
+    avatar: string
+    name: string
+    message: string
+    time: string
+}
+function Message() {
+    return (
+        <div>
+            <img src="" alt=""/>
+        </div>
+    )
+}
+
+export default Message
